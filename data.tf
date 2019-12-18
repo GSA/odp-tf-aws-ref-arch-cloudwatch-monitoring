fa87347147e89387e0a44c8593b2f5feca394dee
Doug Sillex <douglas.sillex@gsa.gov>
2019-12-18 09:07:07 -0500

    Fixes (#3)
    * policy path fixs
    
    * policy path fixs
    
    * policy path fixs
    
    * policy path fixs
    
    * policy path fixs

--- data.tf
+++ data.tf
@@ -2,20 +2,15 @@ data "aws_caller_identity" "current_user" {
 }
 
 data "template_file" "sns_topic_alarms_policy" {
-  template = file(
-    "../policies/templates/sns.json.tpl",
-  )
+  template = "${file("./policies/templates/sns.json.tpl")}"
   vars = {
-    sns_arn = aws_sns_topic.sns_topic_alarms.arn
+    sns_arn = "${aws_sns_topic.sns_topic_alarms.arn}"
   }
 }
 
 data "template_file" "sns_topic_events_policy" {
-  template = file(
-    "../policies/templates/sns.json.tpl",
-  )
+  template = "${file("./policies/templates/sns.json.tpl")}"
   vars = {
-    sns_arn = aws_sns_topic.sns_topic_events.arn
+    sns_arn = "${aws_sns_topic.sns_topic_events.arn}"
   }
 }
-
