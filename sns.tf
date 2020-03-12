828514ff52e0eccd2e0b86faa6461b8cd0f6a53c
Partha <parthasaradhi.abdas@gsa.gov>
2020-03-12 14:23:49 -0500

    Adding tfsec and tflint to cloudwatch module

--- sns.tf
+++ sns.tf
@@ -3,7 +3,7 @@ resource "aws_kms_key" "kms_sns_topic_alarms" {
   deletion_window_in_days = var.deletion_window
   is_enabled              = var.is_enabled
   enable_key_rotation     = var.is_key_rotation_enabled
-  policy = templatefile("${path.module}/policies/templates/cloudwatch_kms.json.tpl",{
+  policy = templatefile("${path.module}/policies/templates/cloudwatch_kms.json.tpl", {
     account_owner = data.aws_caller_identity.current_user.account_id
   })
 }
@@ -18,7 +18,7 @@ resource "aws_kms_key" "kms_sns_topic_events" {
   deletion_window_in_days = var.deletion_window
   is_enabled              = var.is_enabled
   enable_key_rotation     = var.is_key_rotation_enabled
-  policy = templatefile("${path.module}/policies/templates/cloudwatch_kms.json.tpl",{
+  policy = templatefile("${path.module}/policies/templates/cloudwatch_kms.json.tpl", {
     account_owner = data.aws_caller_identity.current_user.account_id
   })
 }
@@ -29,20 +29,20 @@ resource "aws_kms_alias" "kms_sns_topic_events" {
 }
 
 resource "aws_sns_topic" "sns_topic_alarms" {
-  name = var.sns_topic_alarms
+  name              = var.sns_topic_alarms
   kms_master_key_id = aws_kms_key.kms_sns_topic_alarms.key_id
 }
 
 resource "aws_sns_topic" "sns_topic_events" {
-  name = var.sns_topic_events
+  name              = var.sns_topic_events
   kms_master_key_id = aws_kms_key.kms_sns_topic_events.key_id
 }
 
 resource "aws_sns_topic_policy" "sns_policy_alarms" {
   arn = aws_sns_topic.sns_topic_alarms.arn
 
-  policy = templatefile("${path.module}/policies/templates/sns.json.tpl",{
-    sns_arn = aws_sns_topic.sns_topic_alarms.arn
+  policy = templatefile("${path.module}/policies/templates/sns.json.tpl", {
+    sns_arn       = aws_sns_topic.sns_topic_alarms.arn
     account_owner = data.aws_caller_identity.current_user.account_id
   })
 }
@@ -50,8 +50,8 @@ resource "aws_sns_topic_policy" "sns_policy_alarms" {
 resource "aws_sns_topic_policy" "sns_policy_events" {
   arn = aws_sns_topic.sns_topic_events.arn
 
-  policy = templatefile("${path.module}/policies/templates/sns.json.tpl",{
-    sns_arn = aws_sns_topic.sns_topic_events.arn
+  policy = templatefile("${path.module}/policies/templates/sns.json.tpl", {
+    sns_arn       = aws_sns_topic.sns_topic_events.arn
     account_owner = data.aws_caller_identity.current_user.account_id
   })
 }
