e1f7e04d3cc3368a5c81ff3bb92ab01f81ba5537
RobertJLupinek <9854828608@GSA.GOV>
2020-04-14 10:52:12 -0400

    Creating a single SNS topic for alerts and events and correcting issues with CIS benchmark findings.

--- example/main.tf
+++ example/main.tf
@@ -8,5 +8,4 @@ module "cloudwatch_monitoring" {
   appenv                     = "test"
   cloudwatch_log_group       = "grace-cloudtrail" //Not built by this module
   sns_topic_alarms           = "odp-monitoring-alarms"
-  sns_topic_events           = "odp-monitoring-events"
 }
